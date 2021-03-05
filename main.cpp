2c01feab9ea07efb4c541049aedac1b89e53e9d0
Ernir Erlingsson <ernire@gmail.com>
2021-03-05 17:45:33 +0100

    WIP

--- main.cpp
+++ main.cpp
@@ -5,24 +5,25 @@
 #include <fstream>
 #include <functional>
 
-void count_lines_and_dimensions(const std::string &in_file, int &lines, int &dimensions) noexcept {
+void count_lines_and_dimensions(const std::string &in_file, std::size_t &n_elem, std::size_t &n_dim) noexcept {
     std::ifstream is(in_file);
     std::string line, buf;
     int cnt = 0;
-    dimensions = 0;
+    n_dim = 0;
     while (std::getline(is, line)) {
-        if (dimensions == 0) {
+        if (n_dim == 0) {
             std::istringstream iss(line);
             std::vector<std::string> results(std::istream_iterator<std::string>{iss},
                     std::istream_iterator<std::string>());
-            dimensions = results.size();
+            n_dim = results.size();
         }
         ++cnt;
     }
-    lines = cnt;
+    n_elem = cnt;
     is.close();
 }
 
+/*
 bool csv_to_bin(std::string const &in_file, std::string const &out_file, std::function<void(int, int)> const &meta_callback) {
     int n_dim = 0;
     int n_sample = 0;
@@ -62,6 +63,7 @@ bool csv_to_bin(std::string const &in_file, std::string const &out_file, std::fu
     is.close();
     return true;
 }
+ */
 
 std::streampos get_file_size(std::string const &in_file) {
     std::streampos fsize = 0;
@@ -75,7 +77,139 @@ std::streampos get_file_size(std::string const &in_file) {
     return fsize;
 }
 
+/*
+void read_input_hdf5(const std::string &in_file, h_vec<float> &v_points, int &n_coord, int &n_dim,
+        int const n_nodes, int i_node) noexcept {
+#ifdef HDF5_ON
+    //        std::cout << "HDF5 Reading Start: " << std::endl;
+
+
+        // TODO H5F_ACC_RDONLY ?
+        hid_t file = H5Fopen(in_file.c_str(), H5F_ACC_RDWR, H5P_DEFAULT);
+//        hid_t dset = H5Dopen1(file, "DBSCAN");
+//        hid_t dset = H5Dopen1(file, i_node < (n_nodes / 2)? "xyz_1" : "xyz_2");
+        hid_t dset = H5Dopen1(file, "xyz");
+        hid_t fileSpace= H5Dget_space(dset);
+
+        // Read dataset size and calculate chunk size
+        hsize_t count[2];
+        H5Sget_simple_extent_dims(fileSpace, count,NULL);
+        n_coord = count[0];
+        n_dim = count[1];
+//        std::cout << "HDF5 total size: " << n_coord << std::endl;
+
+//        hsize_t block_size =  get_block_size(i_node >= (n_nodes / 2)? i_node - (n_nodes / 2): i_node, n_coord, n_nodes / 2);
+//        hsize_t block_offset =  get_block_offset(i_node >= (n_nodes / 2)? i_node - (n_nodes / 2): i_node, n_coord, n_nodes / 2);
+        hsize_t block_size =  get_block_size(i_node, n_coord, n_nodes);
+        hsize_t block_offset =  get_block_offset(i_node, n_coord, n_nodes);
+//        std::cout << "i_node: " << i_node << " offset:size " << block_offset << " : " <<  block_size << std::endl;
+        hsize_t offset[2] = {block_offset, 0};
+        count[0] = block_size;
+        v_points.resize(block_size * n_dim);
+
+        hid_t memSpace = H5Screate_simple(2, count, NULL);
+        H5Sselect_hyperslab(fileSpace,H5S_SELECT_SET,offset, NULL, count, NULL);
+        H5Dread(dset, H5T_IEEE_F32LE, memSpace, fileSpace,H5P_DEFAULT, &v_points[0]);
+
+        H5Dclose(dset);
+        H5Fclose(file);
+
+#endif
+}
+
+void read_input_bin(const std::string &in_file, h_vec<float> &v_points, int &n_coord, int &n_dim,
+        int const n_nodes, int const i_node) noexcept {
+    std::ifstream ifs(in_file, std::ios::in | std::ifstream::binary);
+    ifs.read((char *) &n_coord, sizeof(int));
+    ifs.read((char *) &n_dim, sizeof(int));
+    auto size = get_block_size(i_node, n_coord, n_nodes);
+    auto offset = get_block_offset(i_node, n_coord, n_nodes);
+    auto feature_offset = 2 * sizeof(int) + (offset * n_dim * sizeof(float));
+    v_points.resize(size * n_dim);
+    ifs.seekg(feature_offset, std::istream::beg);
+    ifs.read((char *) &v_points[0], size * n_dim * sizeof(float));
+    ifs.close();
+}
+ */
+
+void read_input_csv(const std::string &in_file, std::vector<float> &v_data, std::size_t const n_dim, int const sample_rate) noexcept {
+    std::ifstream is(in_file, std::ifstream::in);
+    std::string line, buf;
+    std::stringstream ss;
+    int write_head = 0;
+    int read_head = 0;
+
+    while (std::getline(is, line)) {
+        if (read_head++ % sample_rate > 0)
+            continue;
+        ss.str(std::string());
+        ss.clear();
+        ss << line;
+        for (int j = 0; j < n_dim; j++) {
+            ss >> buf;
+            v_data[write_head++] = static_cast<float>(atof(buf.c_str()));
+        }
+    }
+    is.close();
+}
+
+void print_help() {
+    std::cout << std::endl << std::endl;
+    std::cout << "Usage: [executable] -i [input file] -o [output file] -s [sample rate]" << std::endl;
+    std::cout << "    -s [sample rate] : Optional parameter to subsample 1 in [s] points, e.g. s=2 selects every other element" << std::endl;
+    std::cout << "Supported Input Types:" << std::endl;
+    std::cout << ".csv: Text file with one sample/point per line and features/dimensions separated by a space delimiter" << std::endl;
+    std::cout << ".bin: Custom binary file format" << std::endl;
+    std::cout << ".h5: HDF5 file format" << std::endl;
+}
+
 int main(int argc, char** argv) {
+
+    std::string input_file;
+    std::string output_file;
+    int sample_rate = 1;
+
+    if (argc < 5 || argc % 2 == 0) {
+        std::cerr << "Wrong number of arguments" << std::endl;
+        print_help();
+        exit(0);
+    }
+
+    for (int i = 1; i < argc; i += 2) {
+        std::string str(argv[i]);
+        if (str == "-i") {
+            input_file = argv[i+1];
+        } else if (str == "-o") {
+            output_file = argv[i+1];
+        } else if (str == "-s") {
+            sample_rate = std::stoi(argv[i+1]);
+        }
+    }
+
+    std::vector<float> v_data;
+    std::size_t n_elem, n_dim;
+    if (input_file.find(".csv") != std::string::npos) {
+        count_lines_and_dimensions(input_file, n_elem, n_dim);
+        std::cout << "Found " << n_elem << " elements " << " with " << n_dim << " dimensions" << std::endl;
+        std::cout << "Reading data.." << std::flush;
+        read_input_csv(input_file, v_data, n_dim, sample_rate);
+        std::cout << " done!" << std::endl;
+    } else if (input_file.find(".bin") != std::string::npos) {
+
+    } else if (input_file.find(".h5") != std::string::npos) {
+
+    }
+
+    if (input_file.find(".csv") != std::string::npos) {
+        std::cout << "Writing data.." << std::flush;
+        std::cout << " done!" << std::endl;
+    } else if (input_file.find(".bin") != std::string::npos) {
+
+    } else if (input_file.find(".h5") != std::string::npos) {
+
+    }
+
+    /*
     if (argc != 3) {
         std::cout << "Please specify an input file and an output file (in that order)" << std::endl;
     }
@@ -97,6 +231,7 @@ int main(int argc, char** argv) {
             std::cout << "Error occurred, file has NOT been transformed" << std::endl;
         }
     }
+     */
 
     return 0;
 }
